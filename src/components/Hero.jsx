9af929c75296d66a4dd2094571cff465331b081d
RanaTurab <turabhafiz@gmail.com>
2026-03-14 21:08:40 +0500

    fix capture.png path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
         <div>
             <section class="text-gray-600 body-font bg-gray-200">
                 <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded-2xl drop-shadow-2xl hover:scale-102 " alt="hero" src="public/Capture.png" />
+                    <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded-2xl drop-shadow-2xl hover:scale-102 " alt="hero" src="/Capture.png" />
                     <div class="text-center lg:w-2/3 w-full">
                         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Responsive Across Every Screen</h1>
                         <p class="mb-8 leading-relaxed text-2xl">From desktops to smartphones, I create fully responsive designs that adapt seamlessly to any device. Flexible grids, scalable components, and thoughtful breakpoints ensure consistency and clarity, no matter how users access the platform.</p>
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <footer className="text-gray-600 bg-gray-700 body-font">
                 <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
                     <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 ">
-                        <img src='public/logo.png' className='w-35 rounded-full'></img>
+                        <img src='/logo.png' className='w-35 rounded-full'></img>
                         <span class="ml-3 text-white hover:text-2xl">Syntax studio</span>
                     </a>
                     <p className="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 ">© dev/2026==
