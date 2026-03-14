327fbd91668f3ccc8ff273101adba118b1d94392
RanaTurab <turabhafiz@gmail.com>
2026-03-14 21:29:40 +0500

    fix capture path

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -8,7 +8,7 @@ function Blog() {
                     <div class="flex flex-wrap -m-4">
                         <div class="p-4 md:w-1/3">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-48 md:h-36 w-full object-cover object-center bg-gradient-to-t from-indigo opacity-70 " src="/capture1.jpg" alt="blog" />
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center bg-gradient-to-t from-indigo opacity-70 " src="/Capture1.JPG" alt="blog" />
                                 <div class="p-6">
                                     <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Code & Connect</h1>
@@ -39,7 +39,7 @@ function Blog() {
                         </div>
                         <div class="p-4 md:w-1/3">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/Capture2.jpg" alt="blog" />
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center" src="/Capture2.JPG" alt="blog" />
                                 <div class="p-6">
                                     <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Continuous Growth & Innovation</h1>
@@ -68,7 +68,7 @@ function Blog() {
                         </div>
                         <div class="p-4 md:w-1/3">
                             <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                <img class="lg:h-48 md:h-36 w-full object-cover object-center " src="/Capture3.jpg" alt="blog" />
+                                <img class="lg:h-48 md:h-36 w-full object-cover object-center " src="/Capture3.JPG" alt="blog" />
                                 <div class="p-6">
                                     <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2>
                                     <h1 class="title-font text-lg font-medium text-gray-900 mb-3">Detail-Driven Development</h1>
